34161e2fe43cf253f48502a19d39a14aa2eea5b8
Jack Q <jljqiuc@gmail.com>
2022-01-27 23:43:59 +0000

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,18 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `static/favicon.svg`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-typescript`,
       options: {
         isTSX: true, // defaults to false
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,12 @@ const IndexPage = () => {
     <main className="flex w-screen h-screen justify-center items-center">
       <title>Jack Qiu's website</title>
       <meta name="Jack Qiu's website" content="Jack Qiu's website"></meta>
+      <link
+        rel="icon"
+        href="favicon.svg"
+        sizes="any"
+        type="image/svg+xml"
+      ></link>
       <IntroBox title={name} description={summary} />
     </main>
   );
